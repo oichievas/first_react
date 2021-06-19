c7e892e3c3b08cbdb095aa39a0296bb2a5013844
Сауле Ойчиева <oichievas>
2021-06-19 23:12:52 +0600

    added styles fot footer

---
+++ src/components/Footer.js
@@ -0,0 +1,96 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Button } from "./Button";
+import "./Footer.css";
+
+function Footer(props) {
+  return (
+    <div className="footer-container">
+      <section className="footer-subscription">
+        <p className="footer-subscriprion-heading">
+          Join the Adventure newsletter to receive our best vacation deals
+        </p>
+        <p className="footer-subscription-text">
+          Yo can unsubscribe at any time.
+        </p>
+        <div className="input-areas">
+          <form action="">
+            <input
+              type="email"
+              name="email"
+              placeholder="Your Email"
+              className="footer-input"
+            />
+            <Button buttonStyle="btn--outline">Subscribe</Button>
+          </form>
+        </div>
+      </section>
+      <div className="footer-links">
+        <div className="footer-link-wrapper">
+          <div className="footer-link-items">
+            <h2>About Us</h2>
+            <Link to="/sign-up">How it works</Link>
+            <Link to="/">Testimonials</Link>
+            <Link to="/">Careers</Link>
+            <Link to="/">Investors</Link>
+            <Link to="/">Terms of Service</Link>
+          </div>
+          <div className="footer-link-items">
+            <h2>Contact Us</h2>
+            <Link to="/sign-up">How it works</Link>
+            <Link to="/">Testimonials</Link>
+            <Link to="/">Careers</Link>
+            <Link to="/">Investors</Link>
+            <Link to="/">Terms of Service</Link>
+          </div>
+          <div className="footer-link-items">
+            <h2>About Us</h2>
+            <Link to="/sign-up">How it works</Link>
+            <Link to="/">Testimonials</Link>
+            <Link to="/">Careers</Link>
+            <Link to="/">Investors</Link>
+            <Link to="/">Terms of Service</Link>
+          </div>
+          <div className="footer-link-items">
+            <h2>Contact Us</h2>
+            <Link to="/sign-up">How it works</Link>
+            <Link to="/">Testimonials</Link>
+            <Link to="/">Careers</Link>
+            <Link to="/">Investors</Link>
+            <Link to="/">Terms of Service</Link>
+          </div>
+        </div>
+      </div>
+      <section className="social-media">
+        <div className="social-media-wrap">
+          <div className="footer-logo">
+            <Link className="social-logo">
+              TRVL <i className="fab fa-typo3"></i>
+            </Link>
+          </div>
+          <small className="website-rights"> TRVL © 2021 </small>
+          <div className="social-icons">
+            <Link
+              className="social-icon-link facebook"
+              to="/"
+              target="_blank"
+              aria-label="Facebook"
+            >
+              <i className="fab fa-facebook" />
+            </Link>
+            <Link
+              className="social-icon-link instagram"
+              to="/"
+              target="_blank"
+              aria-label="Facebook"
+            >
+              <i className="fab fa-instagram" />
+            </Link>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default Footer;
